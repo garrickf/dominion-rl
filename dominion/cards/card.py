5e57b209932fa65575cddef9b89a93fae2120ae3
Garrick Fernandez <20538779+garrickf@users.noreply.github.com>
2020-10-28 23:43:51 -0700

    style: reformat card using black

--- dominion/cards/card.py
+++ dominion/cards/card.py
@@ -1,4 +1,4 @@
-""" Card class TODO: docstring
+"""Card class TODO: docstring
 """
 
 from abc import ABC, abstractmethod
@@ -7,7 +7,7 @@ from dominion.common import CardType
 
 class Card(ABC):
     def __init__(self, *, name, kind, cost, desc=""):
-        """ Constructs a card.
+        """Constructs a card.
 
         @param name (str): The card's name
         @param kind (dominion.common.CardType) Card type
@@ -20,37 +20,30 @@ class Card(ABC):
         self.description = desc
 
     def __str__(self):
-        """ Returns a string representation of the card; useful during gameplay.
-        """
-        return '[{}]'.format(self.name)
+        """Returns a string representation of the card; useful during gameplay."""
+        return "[{}]".format(self.name)
 
     @property
     def desc(self):
         if self.description:
             return self.description
         elif self.kind == CardType.VICTORY:
-            return f'Victory card worth %s VP.' % (self.vp)
+            return f"Victory card worth %s VP." % (self.vp)
         elif self.kind == CardType.TREASURE:
-            return f'Treasure card worth %s.' % (self.value)
+            return f"Treasure card worth %s." % (self.value)
         elif self.kind == CardType.CURSE:
-            return f'%s VP for each held.' % (self.vp)
+            return f"%s VP for each held." % (self.vp)
 
 
 class TreasureCard(Card):
     def __init__(self, *, name, cost, desc="", value):
-        super().__init__(name=name,
-                         kind=CardType.TREASURE,
-                         cost=cost,
-                         desc=desc)
+        super().__init__(name=name, kind=CardType.TREASURE, cost=cost, desc=desc)
         self.value = value
 
 
 class VictoryCard(Card):
     def __init__(self, *, name, cost, desc="", vp):
-        super().__init__(name=name,
-                         kind=CardType.VICTORY,
-                         cost=cost,
-                         desc=desc)
+        super().__init__(name=name, kind=CardType.VICTORY, cost=cost, desc=desc)
         self.vp = vp
 
 
